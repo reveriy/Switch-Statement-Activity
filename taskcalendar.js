533b38be477dc3a627325f170bbdef3750c68d3e
Valorie Portem <valorieportem@gmail.com>
2024-10-08 01:56:04 +0800

    git commit -m "initial commit"

---
+++ taskcalendar.js
@@ -0,0 +1,20 @@
+let day = prompt("Enter a day in a week:");
+switch (day) {
+    case "Monday":
+        alert("You have 4 activities due today.");
+        break;
+    case "Tuesday":
+        alert("Do your laundry.");
+        break;
+    case "Wednesday":
+        alert("You have a 3 online class.");
+        break;
+    case "Thursday":
+        alert("You have a dentist appointment.");
+        break;
+    case "Friday":
+        alert("Go take some rest.");
+        break;
+    default:
+        alert("Saturday or Sunday is work day.");
+}
